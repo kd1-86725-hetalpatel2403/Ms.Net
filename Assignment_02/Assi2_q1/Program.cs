987750936baa79f68c55658162522cfec23a4cd2
Hetal Patel <hetalpatel24032002@gmail.com>
2024-12-27 19:43:37 +0530

    Assi 2 added

---
+++ Assignment_02/Assi2_q1/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_02
+{
+    public class Program{ 
+        static void Main (string[] args)
+        {
+            Student student = new Student();
+            student.AcceptDetails();
+            student.PrintDetails();
+
+        }
+    }
+
+    public struct Student
+    {
+        private string Name;
+        private bool Gender;
+        private int Age;
+        private int Std;
+        public char Div;
+        private double Marks;
+
+        //public Student()
+        //{
+        //    Name = string.Empty;
+        //    Gender = true;
+        //    Age = 0;
+        //    Std = 0;
+        //    Div = 'H';
+        //    Marks = 0.0;
+
+        //}
+        public Student(string name,bool gender,int age, int std, char div, double marks)
+        {
+            Name = name;
+            Gender = gender;
+            Age = age;
+            Std = std;
+            Div = div;
+            Marks = marks;
+        }
+
+        public double _Marks
+        {
+            get { return Marks; }
+            set { Marks = value; }
+        }
+        public char _Div
+        {
+            get { return Div; }
+            set { Div = value; }
+        }
+        public int _Std
+        {
+            get { return Std; }
+            set { Std = value; }
+        }
+        public int _Age
+        {
+            get { return Age; }
+            set { Age = value; }
+        }
+
+
+        public bool _Gender
+        {
+            get { return Gender; }
+            set { Gender = value; }
+        }
+
+        public string _Name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+
+        public void AcceptDetails()
+        {
+            Console.WriteLine("Enter your Name: ");
+            this.Name = Console.ReadLine();
+            Console.WriteLine("Enter your Gender: (true for female and false for male)");
+            this.Gender = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Enter your Age: ");
+            this.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Standard: ");
+            this.Std = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Division: ");
+            this.Div = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter your Marks: ");
+            this.Marks = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void PrintDetails()
+        {
+            //return Name.ToString() + " " + Gender.ToString() + " " + Age.ToString() + " "  + Std.ToString()  + " " + Div.ToString() + " " + Marks.ToString();   
+            Console.WriteLine("Name:"+ Name);
+            Console.WriteLine("Gender:" + Gender);
+            Console.WriteLine("Age:" + Age);
+            Console.WriteLine("Standard:" + Std);
+            Console.WriteLine("Division:" + Div);
+            Console.WriteLine("Marks:" + Marks);
+        }
+
+    }
+}
---
+++ Assignment_02/Assi2_q2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assi2_q2
+{
+    public class Student
+    {
+        private string Name;
+        private int Age;
+
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return Name+" "+ Age;
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] s = CreateArray();
+            AcceptInfo(s);
+            PrintInfo(s);
+
+            Student[] reversedStudents = ReverseArray(s);
+            PrintInfo(reversedStudents);
+        }
+    
+    static Student[] CreateArray()
+        {
+            Console.WriteLine("Enter no. of students: ");
+            int nStud = int.Parse(Console.ReadLine());
+            return new Student[nStud];
+        }
+        static void AcceptInfo(Student[] s)
+        {
+            for (int i = 0; i < s.Length; i++) {
+                Console.WriteLine("Enter details of Students: ");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                s[i] = new Student(name, age);
+            }
+        }
+
+        static void PrintInfo(Student[] s)
+        {
+            Console.WriteLine("Students Information: ");
+            foreach (Student student in s) Console.WriteLine(student);
+        }
+
+        static Student[] ReverseArray(Student[] s)
+        {
+            Student[] reversedStudent = new Student[s.Length];
+            for (int i = 0; i < reversedStudent.Length; i++) {
+                reversedStudent[i] = s[s.Length - 1 - i];
+            }
+            return reversedStudent;
+        }
+
+    }
+}
